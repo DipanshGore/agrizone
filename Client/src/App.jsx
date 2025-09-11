ff5af09cec9605e8fd748c9c3cb9613ef17fcaa4
DipanshGore <dgore7078@gmail.com>
2025-09-11 12:21:03 +0530

    react bits

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,12 +1,12 @@
 import { Outlet } from 'react-router-dom'; 
 import Navbar from './components/Navbar';
-import SplashCursor  from '../ReactBits/SplashCursor/SplashCursor.jsx';
+// import SplashCursor  from '../ReactBits/SplashCursor/SplashCursor.jsx';
 
 const App = () => {
   return (
     <div>
       <Navbar/>
-      <SplashCursor />
+      {/* <SplashCursor /> */}
       <Outlet />
     </div>
   );
